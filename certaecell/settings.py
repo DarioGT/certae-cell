7eaf7bd380a5f2ca7d02295ee9eba4e4a48120eb
Victorette <giovanni.victorette@gmail.com>
2014-07-03 13:28:06 -0400

    First commit

---
+++ certaecell/settings.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+#@PydevCodeAnalysisIgnore
+
+"""
+Django settings for certae project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+import site
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+PPATH = BASE_DIR
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+from certaecell.settings_secured import PROTECTED_KEY, MYSQLCONFIG
+SECRET_KEY = PROTECTED_KEY
+DATABASES = MYSQLCONFIG
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'protobase',
+    'protoLib',
+    'prototype',
+    'cell',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'certaecell.urls'
+
+WSGI_APPLICATION = 'certaecell.wsgi.application'
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'fr-ca'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+FILE_UPLOAD_PERMISSIONS = 0644
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+)
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(site.USER_SITE, 'protobase/static'),
+)
+
+SITE_ID = 1
